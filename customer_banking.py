d2714290db199da69a3a43d24b6054595f5ddeed
jaidevkler <jaidev@kler.net>
2024-03-27 13:48:23 -0400

    Modifed customer_banking, saving_account, cd_account as per the assignment

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
-# ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
+import sys
 
 # Define the main function
 def main():
@@ -8,22 +10,37 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    # ADD YOUR CODE HERE
+    try:
+        savings_balance = float(input("Enter savings balance: "))
+        savings_interest = float(input("Enter savings interest rate: "))
+        savings_maturity = int(input("Enter savings mauturity in months: "))
+        if savings_balance < 0 or savings_interest < 0 or savings_maturity < 0:
+            sys.exit('Entered values cannot be negative')
+    except ValueError:
+        sys.exit('Entered value are of an incorrect data type!')
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    print(f"The interest earned is ${interest_earned:,.2f} and the updated savings balance is ${savings_balance:,.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    # ADD YOUR CODE HERE
+    try:
+        cd_balance = float(input("Enter CD balance: "))
+        cd_interest = float(input("Enter CD interest rate: "))
+        cd_maturity = int(input("Enter CD maturity in months: "))
+        if cd_balance < 0 or cd_interest < 0 or cd_maturity < 0:
+            sys.exit('Entered values cannot be negative')
+    except ValueError:
+        sys.exit('Entered value are of an incorrect data type!')
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    print(f"The interest earned is ${interest_earned:,.2f} and the updated CD account balance is ${updated_cd_balance:,.2f}")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
